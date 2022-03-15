6fe74c44c76ab2bd6be32ec375bf0c6a682754df
Kacper 'Gromp' Ducin <69856058+KDucin@users.noreply.github.com>
2022-03-15 09:40:00 +0100

    New Easy Problems
    Add Easy

--- EasyLeetCode.h
+++ EasyLeetCode.h
@@ -163,7 +163,7 @@ int countValidWords(string sentence) {
     return count;
 }
 
-//https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 int removeDuplicates(vector<int>& v){
     int ptr = 0, i = 1, n = v.size();
     while(i < n){
@@ -202,4 +202,22 @@ int prefixCount(vector<string>& words, string pref) {
 //https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
 int isPrefixOfWord(string sentence, string searchWord) {
         
+}
+
+//https://leetcode.com/problems/remove-element/
+int removeElement(vector<int>& nums, int val) {
+    int c=0;
+    int l=0;
+    while(l<nums.size()){
+        if(nums[l]==val){
+            l++;
+            continue;
+        }
+        else{
+            nums[c]=nums[l];
+            c++;
+            l++;
+        }
+    }
+    return c;
 }
